a87148ec972256e7f2c199ed8acd4192b47fd800
tbugar <bugar.tamas94@gmail.com>
2017-11-24 16:26:53 +0100

    MOD search queries

--- szoftarch-recipedb-dao/src/main/java/hu/buko/szoftarchrecipedb/dao/RecipeRepository.java
+++ szoftarch-recipedb-dao/src/main/java/hu/buko/szoftarchrecipedb/dao/RecipeRepository.java
@@ -13,9 +13,9 @@ public interface RecipeRepository extends  MongoRepository<Recipe, String>{
     List<Recipe> findAllByPendingIsTrue();
     List<Recipe> findAllByPendingIsFalse();
     List<Recipe> findAllByPendingIsFalseAndNameLike(String namePart);
-    @Query("{ 'ingredients.name' : {$regex : '^?0$', $options: 'i'}}")
+    @Query("{ 'ingredients.name' : {$regex : '^?0$', $options: 'i'}, pending:false}")
     List<Recipe> findAllByPendingIsFalseAndIngredientsContains(String ingredientPart);
-    @Query("{ 'categories' : ?0 }")
+    @Query("{ 'categories' : ?0 , pending:false}")
     List<Recipe> findAllByCategoriesContains(String categoryName);
     List<Recipe> findByCategoriesAndPendingIsFalse(Category categorie);
 }
